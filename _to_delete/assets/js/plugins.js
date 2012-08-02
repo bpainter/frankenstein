98905dc01553ec0c7389f9de772bed269a56a3f4
Bermon Painter <hello@bermonpainter.com>
2012-08-02 08:01:11 -0400

    Cleaned up the old nanoc install & renamed widgets to module.

--- _to_delete/assets/js/plugins.js
+++
@@ -1,5 +0,0 @@
-// Concatenated file of various plugins
-jQuery(function($) {
-	// Simple function to determine if something exists
-	jQuery.fn.exists = function(){return jQuery(this).length>0;}	
-});
--- _to_delete/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-jQuery(function($) {
-	
-	// Your custom javascript
-
-});
